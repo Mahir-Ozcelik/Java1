ff473c341867643691e31580e216c8c58b008bd5
mhroz <mhr.ozc@hotmail.com>
2024-08-21 20:21:08 +0300

    Task 1 yapıldı

---
+++ src/Gun01/Merhaba.java
@@ -0,0 +1,13 @@
+package Gun01;
+
+public class Merhaba {
+    public static void main(String[] args) {
+        System.out.println("Merhaba 1");
+
+        System.out.println("Merhaba 2");
+
+        System.out.println("Task 1 kodları");
+    }
+}
+// Kırmızı renk : BU DOSYA HENÜZ TAKİP EDİLMİYOR
+// Yeşi renk : BU DOSYA TAKİP EDİLİYOR, YANİ DEĞİŞİKLİKLER KAYDEDİLECEK
