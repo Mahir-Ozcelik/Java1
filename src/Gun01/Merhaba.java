e793c04f031beaeb1d375044841c4bca2750e757
mhroz <mhr.ozc@hotmail.com>
2024-08-21 20:31:35 +0300

    wish list testi eklendi

--- src/Gun01/Merhaba.java
+++ src/Gun01/Merhaba.java
@@ -4,13 +4,17 @@ public class Merhaba {
     public static void main(String[] args) {
         System.out.println("Merhaba 1");
 
-        System.out.println("Merhaba 2");
 
         System.out.println("Task 1 kodları");
 
         System.out.println("Task 2 kodları");
 
         System.out.println("Task 3 kodları");
+
+        for (int i = 0; i <10 ; i++) {
+            System.out.println("Task 4 yapıldı");
+
+        }
     }
 }
 // Kırmızı renk : BU DOSYA HENÜZ TAKİP EDİLMİYOR
